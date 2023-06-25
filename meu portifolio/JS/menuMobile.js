4e8874c73d5c9f7b4f7de8d7ec39e15db3362ae5
Rikelme Santos <rikelmecontv@gmail.com>
2023-06-25 18:08:11 -0300

    responsividade em andamento

--- meu portifolio/JS/menuMobile.js
+++ meu portifolio/JS/menuMobile.js
@@ -1,4 +1,12 @@
 AOS.init();
 export default function MenuMobile() {
+  const btnMenu = document.querySelector('.menu-hamburguer')
+  const menuMobile = document.querySelector('.links-container-mobile')
+
+  function showMenuMobile() {
+    menuMobile.classList.toggle('disable')
+  }
+
+  btnMenu.addEventListener('click', showMenuMobile)
 
 }
